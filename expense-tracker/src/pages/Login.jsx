83b274e28eb92054d8e7acc55dea7953a0b8031a
arayanmemon <memonaryu@gmail.com>
2024-10-21 12:54:24 +0500

    Refactor Login and AddBudget pages

--- expense-tracker/src/pages/Login.jsx
+++ expense-tracker/src/pages/Login.jsx
@@ -6,18 +6,26 @@ function Login() {
     const [password, setPassword] = useState("0");
     return (
         <>
-            <div>
-                <form className="max-w-sm mx-auto" onSubmit={(e)=>{ e.preventDefault(); signInWithEmail(email, password) }}>
+            <div className="py-16">
+                <h2 className="text-center text-3xl py-4">Login</h2>
+                <form className="max-w-sm mx-auto" onSubmit={(e) => { e.preventDefault(); signInWithEmail(email, password) }}>
                     <div className="mb-5">
-                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                        <input type="email" onChange={(e)=> setEmail(e.target.value)} id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@email.com" />
+                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Email</label>
+                        <input type="email" onChange={(e) => setEmail(e.target.value)} id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@email.com" />
                     </div>
                     <div className="mb-5">
-                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                        <input type="password" onChange={(e)=> setPassword(e.target.value)} id="password" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" />
+                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                        <input type="password" onChange={(e) => setPassword(e.target.value)} id="password" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" />
                     </div>
-                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button> <br />
-                    <button onClick={() => signInWithGoogle()}>Login With Google</button>
+                    <div className="flex justify-center">
+                        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button> <br />
+                    </div>
+                        {/* <button onClick={() => signInWithGoogle()} >
+                            <img src="./signin.png" alt="" />
+                        </button> */}
+                    <div className="flex justify-center">
+                        <p>Don't have an account? <a href="/signup" className="text-blue-500">Sign Up</a></p>
+                    </div>
                 </form>
             </div>
 
--- expense-tracker/src/pages/Signup.jsx
+++ expense-tracker/src/pages/Signup.jsx
@@ -1,30 +1,38 @@
 import { useState } from "react";
-import { signUpWithEmail } from "../services/auth.service.js";
+import { signInWithGoogle, signUpWithEmail } from "../services/auth.service.js";
 
 function Signup() {
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
 
-    return ( 
+    return (
         <>
-            <div>
-                <div>
-                    <form className="max-w-sm mx-auto" onSubmit={(e)=>{ e.preventDefault(); signUpWithEmail(email, password) }}>
-                        <div className="mb-5">
-                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                            <input type="email" onChange={(e)=> setEmail(e.target.value)} id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@email.com" />
-                        </div>
-                        <div className="mb-5">
-                            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                            <input type="password" onChange={(e)=> setPassword(e.target.value)} id="password" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" />
-                        </div>
+            <div className="py-16">
+                <h2 className="text-center text-3xl py-4">Sign Up</h2>
+                <form className="max-w-sm mx-auto" onSubmit={(e) => { e.preventDefault(); signUpWithEmail(email, password) }}>
+                    <div className="mb-5">
+                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Email</label>
+                        <input type="email" onChange={(e) => setEmail(e.target.value)} id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@email.com" />
+                    </div>
+                    <div className="mb-5">
+                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                        <input type="password" onChange={(e) => setPassword(e.target.value)} id="password" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" />
+                    </div>
+                    <div className="mb-5">
+                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Confirm Password</label>
+                        <input type="confirmpass"  id="confirmpass" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" />
+                    </div>
+                    <div className="flex justify-center">
                         <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Signup</button> <br />
-                        <button>Signup With Google</button>
-                    </form>
-                </div>
-                <div>
-                    <p>Already have an account? <a href="/login">Login</a></p>
-                </div>
+                    </div>
+
+                    {/* <button onClick={() => signInWithGoogle()}>
+                            <img src="./signup.png" alt="" />
+                        </button> */}
+                    <div className="flex justify-center">
+                        <p>Already have an account? <a href="/login" className="text-blue-500">Login</a></p>
+                    </div>
+                </form> 
             </div>
         </>
     );
