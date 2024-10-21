42146b261961a0fd68cbf26befbcbe7bcac2a590
arayanmemon <memonaryu@gmail.com>
2024-10-21 09:59:17 +0500

    Add configuration files and initial components for Expense Tracker app

---
+++ expense-tracker/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB7KOPzpv8CQxOpizNAbllwm2YjBvrKkxc",
+  authDomain: "expense-tracker-dce0a.firebaseapp.com",
+  projectId: "expense-tracker-dce0a",
+  storageBucket: "expense-tracker-dce0a.appspot.com",
+  messagingSenderId: "1083390162568",
+  appId: "1:1083390162568:web:9c9168b9e5a4b9c5a7113d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ expense-tracker/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='text-center mx-auto w-full'>
+        <h1>Expense Tracker</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
