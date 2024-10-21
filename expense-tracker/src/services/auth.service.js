13528c80545116ada1d86a93d5a386f2934fe022
arayanmemon <memonaryu@gmail.com>
2024-10-21 10:18:07 +0500

    Add firebase dependency to package.json

---
+++ expense-tracker/src/services/auth.service.js
@@ -0,0 +1,31 @@
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
+
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+});
+
+
+signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+});
+
+signInWithRedirect(auth, provider);
