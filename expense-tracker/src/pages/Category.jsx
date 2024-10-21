68ca51beed85cbedb97059f1725bd440b3c98ad5
fahad <fahadzardari111@gmail.com>
2024-10-21 13:11:37 +0500

    addCategory function added

--- expense-tracker/src/pages/Category.jsx
+++ expense-tracker/src/pages/Category.jsx
@@ -1,45 +1,36 @@
 import React from "react";
 import Layout from "../components/Layout";
-
+import { useState } from "react";
+import { addCategoryWithUserId } from "../services/firebase.service";
 export default function Category() {
-
-  
+  const [title, setTitle] = useState("");
+  const userId = JSON.parse(localStorage.getItem("userId"));
   return (
     <Layout>
       <div>
-        <form class="max-w-sm mx-auto">
+        <form class="max-w-sm mx-auto" onSubmit={
+          (e) => {
+            e.preventDefault();
+            addCategoryWithUserId(userId , title);
+          }
+        }>
           <h1 className="text-center text-3xl font-bold">Add Category</h1>
-         
           <div class="mb-5">
-            <label
-              for="title"
-              class="block mb-2 text-sm font-medium "
-            >
+            <label for="title" class="block mb-2 text-sm font-medium ">
               Category Title
             </label>
             <input
               type="title"
               id="title"
               class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-              placeholder="name@flowbite.com"
+              placeholder="Food"
+              value={title}
+              onChange={(e) => {
+                setTitle(e.target.value);
+              } }
               required
             />
           </div>
-          <div class="mb-5">
-            <label
-              for="password"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Your password
-            </label>
-            <input
-              type="password"
-              id="password"
-              class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-              required
-            />
-          </div>
-      
           <button
             type="submit"
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
--- expense-tracker/src/services/firebase.service.js
+++ expense-tracker/src/services/firebase.service.js
@@ -17,8 +17,26 @@ export const addUserDocWithId = async (id, email) => {
   }
 }
 
-export const addCategoryWithUserId = async (id) => {
+export const addCategoryWithUserId = async (id, title) => {
+  try {
+    const colRef = collection(db, "users", id, "categories");
+    await addDoc(colRef, {
+      title: title
+    })
+    window.location.reload();
+  } catch (error) {
+    alert(error);
+  }
+}
 
+export const addBudgetWithUserId = async (id, budget) => {
+  try {
+    const colRef = collection(db, "users", id, "budgets");
+    await addDoc(colRef, budget)
+    window.location.reload();
+  } catch (error) {
+    alert(error);
+  }
 }
 
 // export const getAllCategories = async () => {
